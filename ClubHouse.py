bf11fba2c6d8dd386a71136ed89dda5c48426f8f
Bhavesh Kumar <er.bhavesh@gmail.com>
2020-07-10 14:11:32 +0530

    Add comment

--- ClubHouse.py
+++ ClubHouse.py
@@ -2,103 +2,107 @@
 from Applicant import Applicant
 from HashTable import HashTable
 
-class ClubHouse :
-   
-        
-    def initializeHash(self): 
-        self.ApplicationRecords=HashTable()
-    
-    def insertAppDetails(self, name, phone, memRef, status):
-             p1 = Applicant(name, phone,memRef,status)
-             self.ApplicationRecords.put(p1.name,p1)
-             
-    def updateAppDetails(self, name, phone, memRef, status):
-             p1 = Applicant(name, phone,memRef,status)
-             self.ApplicationRecords.put(p1.name,p1)
-                     
-    def memRef(self, memRefId): 
-        
-        res = [] 
-        for val in self.ApplicationRecords.values(): 
-            if val.memberReferenceId== memRefId : 
-                res.append(val)
-        
-        return res
-        
-    def appStatus(self):
-        
-        applied=0
-        verified=0
-        approved=0
-        
-        for val in self.ApplicationRecords.values():
-            if(val.status=="Applied"):
-                applied=applied+1
-                
-            if(val.status=="Verified"):
-                verified=verified+1
-            if(val.status=="Approved"):
-                approved=approved+1
-            
-            
-        return "Applied: "+str(applied)+ "\n\nVerified : "+str(verified)+"\n\nAproved : "+str(approved)
-    
-    def destroyHash(self): 
-        self.ApplicationRecords.clear()
-        
-        
+def insertAppDetails(ApplicationRecords, name, phone, memRef, status):
+     """This method Insert the applicant record in hash table """
+     p1 = Applicant(name, phone,memRef,status)
+     ApplicationRecords.records.put(p1.name,p1)
+
+def updateAppDetails(ApplicationRecords, name, phone, memRef, status):
+    """This method will update the value based on key , if key is same and its present in hash table than it will update the record 
+    otherwise will insert."""
+    p1 = Applicant(name, phone,memRef,status)
+    ApplicationRecords.records.put(p1.name,p1)
+
+def memRef(ApplicationRecords, memRefId):
+    """This method will serach the applicant based on member reference id. """
+    res = []
+    for val in ApplicationRecords.records.values():
+        if val.memberReferenceId== memRefId :
+              res.append(val)
+
+    return res
+
+def appStatus(ApplicationRecords):
+    """This method will print the statics of applicant status."""
+    applied=0
+    verified=0
+    approved=0
+
+    for val in ApplicationRecords.records.values():
+        if(val.status=="Applied"):
+            applied=applied+1
+        if(val.status=="Verified"):
+            verified=verified+1
+        if(val.status=="Approved"):
+            approved=approved+1
+
+
+    return "Applied: "+str(applied)+ "\n\nVerified : "+str(verified)+"\n\nAproved : "+str(approved)
+
+def destroyHash(ApplicationRecords):
+    """This method will clean the record from hash table."""
+    ApplicationRecords.records.clear()
+
+class ApplicationRecords :
+
+    def initializeHash(self):
+        self.records=HashTable()
+
+
 def main():
-    clubHouse=ClubHouse()
-    clubHouse.initializeHash()
-    with open("inputPS8.txt", "r") as inputFile:
-        with open("outputPS8.txt", "w") as outFile:
-            with open("promptsPS8.txt", "r") as promptsFile:
-    
-                __process_insertion__(inputFile ,outFile,clubHouse)
-                
+
+    appRecords=ApplicationRecords()
+    appRecords.initializeHash()
+
+    with open("inputPS8.txt", "r") as inputFile: #Open input file. 
+        with open("outputPS8.txt", "w") as outFile: # Open output file.
+            with open("promptsPS8.txt", "r") as promptsFile: # open prompt file.
+
+                __process_insertion__(inputFile ,outFile,appRecords) # Call thie medthod to process file data 
+
                 for line in promptsFile:
-                   
+
                     currentline = line.strip().split(":")
-                   
-                    if(currentline[0].strip()=="Update"):
-                        __process_update__(outFile,clubHouse,currentline)
-                       
-                      
-                    if(currentline[0].strip()=="memberRef") :   
-                        __process_member_reference__(outFile,clubHouse,currentline)
-                        
-                    if(currentline[0].strip()=="appStatus"):  
-                        __process_app_status__(outFile,clubHouse)
-    clubHouse.destroyHash()   
-       
-           
-def __process_member_reference__(outFile ,clubHouse,currentline) :
+
+                    if(currentline!= None and len(currentline)>1  and currentline[0].strip()=="Update"):
+                        __process_update__(outFile,appRecords,currentline)
+                    elif (currentline!= None and len(currentline)>1  and currentline[0].strip()=="memberRef"):
+                        __process_member_reference__(outFile,appRecords,currentline)
+
+                    elif(currentline!= None and len(currentline)>1  and currentline[0].strip()=="appStatus"):
+                        __process_app_status__(outFile,appRecords)
+    
+    destroyHash(appRecords) # clean the hash table
+
+
+def __process_member_reference__(outFile ,appRecords,currentline) :
+    """This method retrive the list of applicant based on member reference number.and write into the output. """
     outFile.write("---------- Member reference by "+currentline[1]+" ----------\n\n")
-    apps=clubHouse.memRef(currentline[1].strip())
+    apps=memRef(appRecords,currentline[1].strip())
     for val in apps:
         outFile.write(val.name+" / "+val.phoneNumber+" / " +val.status+"\n\n")
         outFile.write("-------------------------\n\n")
-    
-def __process_app_status__(outFile ,clubHouse) :
+
+def __process_app_status__(outFile ,appRecords) :
+    """This method get the application status and write into the output."""
     outFile.write("-----------Application Status--------------\n\n")
-    outFile.write(clubHouse.appStatus()+"\n\n")
+    outFile.write(appStatus(appRecords)+"\n\n")
     outFile.write("-------------------------\n\n")
-    
-def __process_update__(outFile ,clubHouse,currentline) :
+
+def __process_update__(outFile ,appRecords,currentline) :
     applicant=currentline[1].split("/")
-    clubHouse.updateAppDetails(applicant[0], applicant[1],applicant[2],applicant[3])
+    updateAppDetails(appRecords,applicant[0], applicant[1],applicant[2],applicant[3])
     outFile.write("Updated details of "+applicant[0]+". Application Status has been changed.\n\n")
-    
-def __process_insertion__(inputFile ,outFile,clubHouse) :
+
+def __process_insertion__(inputFile ,outFile,appRecords) :
     i=0
     for line in inputFile:
         i=i+1
         currentline = line.strip().split("/")
-        clubHouse.insertAppDetails(currentline[0], currentline[1],currentline[2],currentline[3])
-    outFile.write("Successfully inserted "+str(i)+"  applications into the system.\n\n") 
-    
-         
+        insertAppDetails(appRecords,currentline[0], currentline[1],currentline[2],currentline[3])
+    outFile.write("Successfully inserted "+str(i)+"  applications into the system.\n\n")
+
+
 if __name__ == "__main__":
-    main()   
-    
-         
+    main()
+
--- HashTable.py
+++ HashTable.py
@@ -2,28 +2,31 @@
 
 ############ HashTable helper functions
 def hashId(key, size):
+    """This function itereate the string and get the ASCI code of each character and do the sumation of asci code and 
+    than take the modelo with size. it will give the array index where the key and value will be present. """
     return sum([ord(c) for c in key]) % size
 
 
 ############ HashTable
 class HashTable:
-   
+    """Hash Table holds the key value pair , the initial capacity of the hash map is defined as 100."""
 
     def __init__(self, capacity=100):
-        """ Capacity defaults to 100. """
-
+        """ This method initialize the Hash table with capacity 100. size 
+        0 , key array as empty and entry as a array of size of capacity."""
         self.capacity = capacity
         self.size = 0
         self._keys = []
-       
-        self._values = [[] for _ in range(capacity)]
+        self._entry = [[] for _ in range(capacity)]
 
     def _find_by_key(self, key, find):
-        index = hashId(key, self.capacity)
+        """Here ke is a key against which against get the value  find is the function which has passed as closer , 
+        the defination of find function will be in the get and put methd from where this function will get called."""
+        index = hashId(key, self.capacity) # Get the index/ bucket based on hash code of the key
         
-        hash_table_cell = self._values[index]
+        hash_table_cell = self._entry[index]
         found_item = None
-        for item in hash_table_cell:
+        for item in hash_table_cell: #Iterrate the entry array and check the key is matching  and if key is same than get the value
             if item[0] == key:
                 found_item = item
                 break
@@ -31,9 +34,12 @@ class HashTable:
         return find(found_item, hash_table_cell)
 
     def put(self, key, obj):
+        """This method get the value against the key """
         
 
-        def find_result_func(found_item, hash_table_cell):
+        def find(found_item, hash_table_cell):
+            """This function is a closer function which will pass to the find_by_key function to get the value. the purpose of this 
+            function is to check if key is already present than replace the value else append the value in same bucket."""
             if found_item:
                 found_item[1] = obj
             else:
@@ -41,58 +47,48 @@ class HashTable:
                 self.size += 1
                 self._keys.append(key)
 
-        self._find_by_key(key, find_result_func)
+        self._find_by_key(key, find)
         return self
 
-    def get(self, key):
-       
-        def find_result_func(found_item, _):
+    def get(self, key, default=None):
+        """This method retrive the value based on key.  """
+        def find(found_item, _):
+            """ This is the closer function which will be passed to find by key function , if key found than return the value 
+            otherwise return blanck"""
             if found_item:
                 return found_item[1]
             else:
-                return ''
+                return default
 
-        return self._find_by_key(key, find_result_func)
+        return self._find_by_key(key, find)
 
-    def remove(self, key):
+    def remove(self, key, default=None):
        
-        def find_result_func(found_item, hash_table_cell):
+        def find(found_item, hash_table_cell):
+            """This function delete the key and entr if found and return the removed entry or return the blanck.  """
             if found_item:
                 hash_table_cell.remove(found_item)
                 self._keys.remove(key)
                 self.size -= 1
                 return found_item[1]
             else:
-                return ""
+                return default
 
-        return self._find_by_key(key, find_result_func)
+        return self._find_by_key(key, find)
     
     def clear(self):
         self._keys.clear()
-        self._values.clear()
+        self._entry.clear()
         self.size=0
    
     ####### Python's dict interface
-
 
     def keys(self):
         return self._keys
 
     def values(self):
         res = [] 
-        for val in self._values: 
+        for val in self._entry: 
             if val != [] : 
                 res.append(self.get(val[0][0]))
-        
         return res
-
-  
-
-    def __repr__(self):
-        return '{ ' + ', '.join([key + ':' + str(self.get(key)) for key in self._keys]) + ' }'
-
-if __name__ == "__main__":
-    # Run unit tests
-    import unittest
-    testsuite = unittest.TestLoader().discover('test', pattern="*hashtable*")
-    unittest.TextTestRunner(verbosity=1).run(testsuite)
